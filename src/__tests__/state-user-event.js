84d8c78bc9b0337dbc7caeee5a8a5ac7b84ede89
oybek703 <hhoybek@gmail.com>
2021-03-07 04:03:10 +0500

    user events tested

--- src/__tests__/state-user-event.js
+++ src/__tests__/state-user-event.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import {FavoriteNumber} from "favorite-number"
-import {fireEvent, getByRole, render} from "@testing-library/react"
+import user from '@testing-library/user-event'
+import {render} from "@testing-library/react"
 
 test('entering invalid number shows an error message', () => {
-    const {getByLabelText, getByRole} = render(<FavoriteNumber/>)
+    const {getByLabelText, getByRole, rerender, queryByRole} = render(<FavoriteNumber/>)
     const input = getByLabelText(/favorite number/i)
-    fireEvent.change(input, {target: {value: 10}})
+    user.type(input, '10')
     expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
+    rerender(<FavoriteNumber max={10}/>)
+    expect(queryByRole('alert')).toBeNull()
 })
