f52d53597265fa7906d502440446f086f161f4cc
oybek703 <hhoybek@gmail.com>
2021-03-08 04:06:24 +0500

    mocking http requests with jest

--- src/__tests__/http-jest-mock.js
+++ src/__tests__/http-jest-mock.js
@@ -1,22 +1,20 @@
-import * as React from 'react'
-import {render, screen, waitFor} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import {loadGreeting as mockLoadGreeting} from '../api'
-import {GreetingLoader} from '../greeting-loader-01-mocking'
+import React from 'react'
+import {render, fireEvent, waitFor} from '@testing-library/react'
+import {GreetingLoader} from "greeting-loader-01-mocking"
+import {loadGreeting as mockLoadGreeting} from "../api"
 
 jest.mock('../api')
 
-test('loads greetings on click', async () => {
+test('loads greeting on click', async() => {
   const testGreeting = 'TEST_GREETING'
   mockLoadGreeting.mockResolvedValueOnce({data: {greeting: testGreeting}})
-  render(<GreetingLoader />)
-  const nameInput = screen.getByLabelText(/name/i)
-  const loadButton = screen.getByText(/load/i)
-  userEvent.type(nameInput, 'Mary')
-  userEvent.click(loadButton)
-  expect(mockLoadGreeting).toHaveBeenCalledWith('Mary')
+  const {getByLabelText, getByText} = render(<GreetingLoader/>)
+  const nameInput = getByLabelText(/name/i)
+  const loadButton = getByText(/load/i)
+  nameInput.value = 'USERNAME'
+  fireEvent.click(loadButton)
+  expect(mockLoadGreeting).toHaveBeenCalledWith('USERNAME')
   expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
-  await waitFor(() =>
-    expect(screen.getByLabelText(/greeting/i)).toHaveTextContent(testGreeting),
-  )
+  await waitFor(() => expect(getByLabelText(/greeting/))
+      .toHaveTextContent(testGreeting))
 })
