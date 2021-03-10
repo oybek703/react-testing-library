63726535eda0e98ca05f754293d2378b6a1b66e4
oybek703 <hhoybek@gmail.com>
2021-03-10 17:01:13 +0500

    tdd with red, green, refactor on jest

--- src/__tests__/tdd-01-markup.js
+++ src/__tests__/tdd-01-markup.js
@@ -1,13 +1,44 @@
 import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
+import {render, fireEvent, waitFor} from '@testing-library/react'
+import {Redirect} from 'react-router'
+import {build, fake, sequence} from 'test-data-bot'
+import {savePost as mockSavePost} from '../api'
 import {Editor} from '../post-editor-01-markup'
 
-test('renders a form with title, content, tags and submit button', () => {
+jest.mock('../api')
+jest.mock('react-router', () => ({
+  Redirect: jest.fn(() => null)
+}))
+afterEach(() => jest.clearAllMocks())
+
+const postBuilder = build('Post').fields({
+  title: fake(f => f.lorem.words()),
+  content: fake(f => f.lorem.paragraphs()),
+  tags: fake(f => [f.lorem.words(), f.lorem.words(), f.lorem.words() ])
+})
+
+const userBuilder = build('User').fields({
+  id: sequence(s => `user-${s}`)
+})
+
+test('renders a form with title, content, tags and submit button', async () => {
+  mockSavePost.mockResolvedValueOnce()
   const {getByText, getByLabelText} = render(<Editor/>)
-  getByLabelText(/title/i)
-  getByLabelText(/content/i)
-  getByLabelText(/tags/i)
+  const fakeUser = userBuilder()
+  const preDate = new Date().getTime()
+  const fakePost = postBuilder()
+  getByLabelText(/title/i).value = fakePost.title
+  getByLabelText(/content/i).value = fakePost.content
+  getByLabelText(/tags/i).value = fakePost.tags.join(', ')
   const submitButton = getByText(/submit/i)
   fireEvent.click(submitButton)
   expect(submitButton).toBeDisabled()
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
+  const postDate = new Date().getTime()
+  const date = new Date(mockSavePost.mock.calls[0][0].date).getTime()
+  expect(date).toBeGreaterThanOrEqual(preDate)
+  expect(date).toBeLessThanOrEqual(postDate)
+  await waitFor(() =>
+      expect(mockSavePost).toHaveBeenCalledWith({...fakePost, authorId: fakeUser.id}))
+  expect(Redirect).toHaveBeenCalledWith({to: '/'}, {})
 })
--- src/post-editor-01-markup.js
+++ src/post-editor-01-markup.js
@@ -1,23 +1,37 @@
 import React, {useState} from 'react'
+import {savePost} from "api"
+import {Redirect} from "react-router"
 
 
 function Editor() {
     const [isSaving, setIsSaving] = useState(false)
+    const [isSaved, setIsSaved] = useState(false)
     function handleSubmit(e) {
         e.preventDefault()
+        const {title, content, tags} = e.target.elements
         setIsSaving(true)
+        savePost({
+            title: title.value,
+            content: content.value,
+            tags: tags.value.split(', '),
+            authorId: 'user-1',
+            date: new Date().toISOString()
+        }).then(() => setIsSaved(true))
+    }
+    if(isSaved) {
+        return <Redirect to='/' />
     }
     return <form onSubmit={handleSubmit}>
         <label htmlFor="title">Title</label>
-        <input type="text" id="title"/>
+        <input name="title" type="text" id="title"/>
 
         <label htmlFor="content">Content</label>
-        <textarea id="content" cols="30" rows="10"></textarea>
+        <textarea name="content" id="content" cols="30" rows="10"></textarea>
 
         <label htmlFor="tags">Tags</label>
-        <textarea id="tags" cols="30" rows="10"></textarea>
+        <textarea name="tags" id="tags" cols="30" rows="10"></textarea>
 
-        <button disabled={isSaving}>Submit</button>
+        <button disabled={isSaving} type='submit'>Submit</button>
     </form>
 }
 
