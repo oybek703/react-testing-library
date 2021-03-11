914a4ac3be72c6b67db29120453ec4a9b0fd8c7d
oybek703 <hhoybek@gmail.com>
2021-03-12 00:13:17 +0500

    timeouts tested with jest

--- src/__tests__/unmounting.js
+++ src/__tests__/unmounting.js
@@ -1,24 +1,25 @@
-import * as React from 'react'
-import {render, act} from '@testing-library/react'
-import {Countdown} from '../countdown'
+import React from 'react'
+import {act, render} from "@testing-library/react"
+import {Countdown} from "countdown"
 
 beforeAll(() => {
   jest.spyOn(console, 'error').mockImplementation(() => {})
 })
 
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
 afterAll(() => {
   console.error.mockRestore()
 })
+test(
+    'does not attempts to set state when unmounted (to prevent memory leaks)',
+    () => {
+      jest.useFakeTimers()
+      const {unmount} = render(<Countdown/>)
+      unmount()
+      act(() => jest.runOnlyPendingTimers())
+      expect(console.error).not.toHaveBeenCalled()
 
-afterEach(() => {
-  jest.clearAllMocks()
-  jest.useRealTimers()
-})
-
-test('does not attempt to set state when unmounted (to prevent memory leaks)', () => {
-  jest.useFakeTimers()
-  const {unmount} = render(<Countdown />)
-  unmount()
-  act(() => jest.runOnlyPendingTimers())
-  expect(console.error).not.toHaveBeenCalled()
 })
