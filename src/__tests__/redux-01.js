7d0b9f81cd044c60cc8b687746b5613ebd662ee6
oybek703 <hhoybek@gmail.com>
2021-03-11 15:17:00 +0500

    redux tested with store

--- src/__tests__/redux-01.js
+++ src/__tests__/redux-01.js
@@ -1,16 +1,29 @@
-import * as React from 'react'
-import {Provider} from 'react-redux'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import {Counter} from '../redux-counter'
-import {store} from '../redux-store'
+import React from 'react'
+import {render as rtlRender, fireEvent} from '@testing-library/react'
+import {Counter} from "redux-counter"
+import {Provider} from "react-redux"
+import {store} from "redux-store"
+import {createStore} from "redux"
+import {reducer} from "redux-reducer"
 
-test('can render with redux with defaults', () => {
-  render(
-    <Provider store={store}>
-      <Counter />
-    </Provider>,
-  )
-  userEvent.click(screen.getByText('+'))
-  expect(screen.getByLabelText(/count/i)).toHaveTextContent('1')
+function render(
+    ui,
+    {initialState = store, ...options} = {}) {
+  function Wrapper() {
+    return <Provider store={initialState}>{ui}</Provider>
+  }
+  return rtlRender(ui, {wrapper: Wrapper, ...options})
+}
+
+test('can render redux with defaults', () => {
+  const {getByText, getByLabelText} = render(<Counter/>)
+  fireEvent.click(getByText('+'))
+  expect(getByLabelText('count')).toHaveTextContent('1')
+})
+
+test('can render redux with custom initial state', () => {
+  const customStore = createStore(reducer, {count: 3})
+  const {getByText, getByLabelText} = render(<Counter/>, {initialState: customStore})
+  fireEvent.click(getByText('-'))
+  expect(getByLabelText('count')).toHaveTextContent('2')
 })
