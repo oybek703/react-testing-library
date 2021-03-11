f5c654d1b7c1b8ef85bfd5b4490fb289700c6872
oybek703 <hhoybek@gmail.com>
2021-03-11 18:28:27 +0500

    react modal tested

--- src/__tests__/portals.js
+++ src/__tests__/portals.js
@@ -1,13 +1,9 @@
-import * as React from 'react'
-import {render, within} from '@testing-library/react'
-import {Modal} from '../modal'
+import React from "react"
+import {Modal} from "modal"
+import {render, within} from "@testing-library/react"
 
 test('modal shows the children', () => {
-  render(
-    <Modal>
-      <div data-testid="test" />
-    </Modal>,
-  )
+  render(<Modal><div data-testid="test"/></Modal>)
   const {getByTestId} = within(document.getElementById('modal-root'))
   expect(getByTestId('test')).toBeInTheDocument()
 })
