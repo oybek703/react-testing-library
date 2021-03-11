a3cc82867323c3ab8b7c57300b3a55ddcc8f15e7
oybek703 <hhoybek@gmail.com>
2021-03-11 05:46:38 +0500

    no-match route tested

--- src/__tests__/react-router-01.js
+++ src/__tests__/react-router-01.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter, Router} from 'react-router-dom'
 import {render, fireEvent} from '@testing-library/react'
 import {Main} from "main"
+import {createMemoryHistory} from "history"
 
 test('main renders about and home and i can navigate to those pages', () => {
   const {getByRole, getByText} = render(<BrowserRouter><Main/></BrowserRouter>)
@@ -9,4 +10,10 @@ test('main renders about and home and i can navigate to those pages', () => {
   expect(getByText(/about/i)).toHaveTextContent(/about/i)
   fireEvent.click(getByText(/about/i))
   expect(getByRole('heading')).toHaveTextContent(/about/i)
+})
+
+test('renders no match route on unmatched route', () => {
+  const history = createMemoryHistory({initialEntries: ['/something-do-not-match']})
+  const {getByRole} = render(<Router history={history}><Main/></Router>)
+  expect(getByRole('heading')).toHaveTextContent(/404/i)
 })
